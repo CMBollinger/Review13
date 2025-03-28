fcc97305f3785931d4946afaa64e7ecde4689b3a
CMBollinger <cmbollinger19@gmail.com>
2025-03-28 10:04:54 -0500

    Finished

--- Scripts/co_cart.js
+++ Scripts/co_cart.js
@@ -28,9 +28,51 @@
    
 */ 
 
+window.addEventListener("load", function() {
+   var cart = document.forms.cart;
+
+   calcCart();
+
+   //Event handler for the web form
+   cart.elements.modelQty.onchange = calcCart;
+
+   for (i=0; i < shippingOptions.length; i++) {
+      shippingOptions[i].onclick = calcCart;
+   }
+});
 
 
+function calcCart() {
+   var cart = document.forms.cart;
 
+   //Calculate initial cost
+   var mCost = cart.elements.modelCost.value;
+   var qIndex = cart.elements.modelQty.selectedIndex;
+   var quantity = cart.elements.modelQty[qIndex].value;
+
+
+   //Initial cost = model cost * qty
+   var orderCost = mCost * quantity;
+   cart.elements.orderCost.value = formatUSCurrency(orderCost);
+
+   //Retrieve the cost of shipping
+   var shipCost = document.querySelector('input[name="shipping"]:checked').value*quantity;
+   cart.elements.shippingCost.value = formatNumber(shipCost, 2);
+
+   //calculate the subtotal
+   cart.elements.subTotal.value = formatNumber(orderCost + shipCost, 2);
+
+   //calculate the cost of salestax
+   var salesTax = 0.05*(orderCost + shipCost);
+   cart.elements.salesTax.value = formatNumber(salesTax, 2);
+
+   //calculate total cost
+   var cartTotal = orderCost + shipCost + salesTax;
+   cart.elements.cartTotal.value = formatUSCurrency(cartTotal);
+
+   //store the order details
+   cart.elements.shippingType.value = document.querySelector('input[name="shipping"]:checked').nextSibling.nodeValue;
+}
 
 
 
